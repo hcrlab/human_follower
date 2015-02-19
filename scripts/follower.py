eac58edc0184b25ffa7b6bd4aa577077ead5400c
Paul Yau <yaup@softshell.(none)>
2015-02-19 15:59:01 -0800

    Fixed some importing errors, and mistructure of goals.

--- scripts/follower.py
+++ scripts/follower.py
@@ -6,18 +6,22 @@ import actionlib
 
 from people_msgs.msg import PositionMeasurementArray
 from geometry_msgs.msg import *
-from move_base_msgs import MoveBaseActionGoal
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 def callback(data):
     # publish to whatever message the driving is going to take place
     client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-    client.wait_for_server()
+    #client.wait_for_server()
     
     if len(data.people) > 0:
-        target_pose = MoveBaseGoal()
-        target_pose.pose.position = data.people[0].pos
-
-        client.send_goal(target_pose)
+        #person found
+        rospy.loginfo("Found person, generate goal")
+        target_goal = MoveBaseGoal()
+        target_goal.target_pose.pose.position = data.people[0].pos
+        
+        #sending goal
+        rospy.loginfo("sending goal")
+        client.send_goal(target_goal)
     
 def follower():
     rospy.init_node('human_follower')
