dfeb8d0b2e333153f443078d652929289e3797f1
Paul Yau <yaup@softshell.(none)>
2015-02-26 16:00:48 -0800

    now just publish to move_base_simple/goal instead of using the actionlib.

--- scripts/follower.py
+++ scripts/follower.py
@@ -10,18 +10,28 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 def callback(data):
     # publish to whatever message the driving is going to take place
+    pub = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size = 10)
     client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
     #client.wait_for_server()
     
     if len(data.people) > 0:
         #person found
         rospy.loginfo("Found person, generate goal")
-        target_goal = MoveBaseGoal()
-        target_goal.target_pose.pose.position = data.people[0].pos
+        target_goal_simple = PoseStamped()
+        #target_goal = MoveBaseGoal()
+        
+        #forming a proper PoseStamped message
+        target_goal_simple.pose.position = data.people[0].pos
+        target_goal_simple.pose.position.z = 0
+        target_goal_simple.pose.orientation.w = 1
+        target_goal_simple.header.frame_id = 'base_link'
+        target_goal_simple.header.stamp = rospy.Time.now()
+        #target_goal.target_pose.pose.position = data.people[0].pos
         
         #sending goal
         rospy.loginfo("sending goal")
-        client.send_goal(target_goal)
+        pub.publish(target_goal_simple)
+        #client.send_goal(target_goal)
     
 def follower():
     rospy.init_node('human_follower')
