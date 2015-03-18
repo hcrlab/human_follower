38ca2db6309157285202e6e0adaf985e80eea990
Paul Yau <yaup@softshell.(none)>
2015-03-18 13:23:15 -0700

    calculated for target to be a little distance away from the person so it's not colliding with the person. TF transformation currently not working

--- scripts/follower.py
+++ scripts/follower.py
@@ -3,35 +3,60 @@
 import roslib
 import rospy
 import actionlib
+import tf
 
 from people_msgs.msg import PositionMeasurementArray
 from geometry_msgs.msg import *
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
+# distances in meters
+DIST_FROM_TARGET = .2
+
 def callback(data):
     # publish to whatever message the driving is going to take place
     pub = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size = 10)
-    client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-    #client.wait_for_server()
     
     if len(data.people) > 0:
         #person found
-        rospy.loginfo("Found person, generate goal")
+        rospy.loginfo("Found person, generating goal")
+        listener = tf.TransformListener()
         target_goal_simple = PoseStamped()
-        #target_goal = MoveBaseGoal()
         
         #forming a proper PoseStamped message
-        target_goal_simple.pose.position = data.people[0].pos
-        target_goal_simple.pose.position.z = 0
-        target_goal_simple.pose.orientation.w = 1
-        target_goal_simple.header.frame_id = 'map'
-        target_goal_simple.header.stamp = rospy.Time.now()
-        #target_goal.target_pose.pose.position = data.people[0].pos
+        try:
+            (trans, rot) = listener.lookupTransform('/base_link', '/map', rospy.Time.now())
+            rospy.loginfo("Transform obtained x:" + trans[0] + " y:" + trans[1])
 
-        #sending goal
-        rospy.loginfo("sending goal")
-        pub.publish(target_goal_simple)
-        #client.send_goal(target_goal)
+            # This is where the target person's legs are
+            legPosition = data.people[0].pos
+            
+            rospy.loginfo("Computing target")
+            # computing target point that is .2 meters away 
+            difference = Point()
+            difference.x = legPosition.x - trans[0]
+            defference.y = legPosition.y - trans[1]
+            
+            angle = math.atan(difference.y, difference.y)
+            length = sqrt(difference.x ** 2 + difference.y ** 2)
+            
+            target_length = length - DIST_FROM_TARGET
+
+            targetX = target_length * math.cos(angle)
+            targetY = target_length * math.sin(angle)
+
+            target_goal_simple.pose.position.x = targetX
+            target_goal_simple.pose.position.y = targetY
+            target_goal_simple.pose.position.z = 0
+            target_goal_simple.pose.orientation.w = 1
+            target_goal_simple.header.frame_id = 'map'
+            target_goal_simple.header.stamp = rospy.Time.now()
+
+            # sending goal
+            rospy.loginfo("sending goal")
+            pub.publish(target_goal_simple)
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.loginfo("failed to get tf transform")
     
 def follower():
     rospy.init_node('human_follower')
