19909012c7c1e0ab13c00d96fe08350bd03a3c16
Paul Yau <yaup@softshell.(none)>
2015-04-02 16:58:32 -0700

    goal orientation is now pointed towards the person. Updated logic on how to detec people. Now only considers target_id and distance from last known position

--- scripts/follower.py
+++ scripts/follower.py
@@ -13,9 +13,9 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 # constants
 DIST_FROM_PREVIOUS = .3 # how close is too close that robot won't send a new goal
-DIST_FROM_TARGET = .4 # how far away the robot should stop from the target
-DIST_NEXT_GOAL = .7 # how far the next goal should idealy be
-PROXIMITY_RELIABILITY = .4 # how far from DIST_NEXT_GOAL is going to bring extra reliaility
+DIST_FROM_TARGET = .6 # how far away the robot should stop from the target
+PROXIMITY_MAX = .4 # how far from last known position leg detector should consider to be probable
+R_SCALE = .5
 RELIABILITY_MIN = .4 #minimum reliability of the position
 
 class ListenerSingleton:
@@ -36,6 +36,7 @@ class HumanFollower:
 
     def __init__(self):
         self.previousGoal = None
+        self.lastKnownPosition = None
         self.trackedObjectID = "Steve"
 
     def callback(self,data):
@@ -45,7 +46,7 @@ class HumanFollower:
         
         if len(data.people) > 0:
             # selecting most probable person
-            rospy.loginfo("Looking for suitble target")
+            rospy.loginfo("Looking for suitible target")
 
             maxReliability = RELIABILITY_MIN
             personIndex = -1
@@ -55,7 +56,8 @@ class HumanFollower:
                 # reliability metric is based on a combination of leg_detector results
                 # and how far this current goal is from the pervious goal.
                 # if the same person is still in sight, it is the most reliable
-                # If there is no previous goal, then it's simply the leg_detector results
+                # If there is no previous goal, then it's simply the leg_detector reliability
+
                 
                 if (self.previousGoal == None):
                     reliability = data.people[i].reliability
@@ -64,19 +66,27 @@ class HumanFollower:
                         reliability = 100
                     else:
                         currPersonPosition = data.people[i].pos
-                        distFromPreviousGoalX = currPersonPosition.x - self.previousGoal.pose.position.x
-                        distFromPreviousGoalY = currPersonPosition.y - self.previousGoal.pose.position.y
-                        distFromPreviousGoal = math.hypot(distFromPreviousGoalX, distFromPreviousGoalY)
-                        distError = abs(distFromPreviousGoal - DIST_NEXT_GOAL) # how far this goal is from the preffered distance
-                        reliability = data.people[i].reliability + (PROXIMITY_RELIABILITY - distError)
 
+                        distFromLastKnownX = currPersonPosition.x - self.lastKnownPosition.pose.position.x
+                        distFromLastKnownY = currPersonPosition.y - self.lastKnownPosition.pose.position.y
+                        distFromLastKnown = math.hypot(distFromLastKnownX, distFromLastKnownY)
 
+                        if (distFromLastKnown < PROXIMITY_MAX):
+                            reliability = data.people[i].reliability + ((PROXIMITY_MAX - distFromLastKnown) * R_SCALE)
+                        else:
+                            reliability = data.people[i].reliability
+                        
                 if (reliability > maxReliability):
                     maxReliability = reliability
                     personIndex = i
-            
+
+            rospy.loginfo("count: " + str(len(data.people)))
+            rospy.loginfo("final R: " + str(reliability))
+            rospy.loginfo("max R: " + str(maxReliability))
+                    
+            # someone more probable than the mmin probability.
             if (personIndex != -1):
-                rospy.loginfo("Found target, generating goal")
+                rospy.loginfo("Target Found")
                 try:
                     listener = ListenerSingleton.new()
                     (trans, rot) = listener.lookupTransform('/map', '/base_link', rospy.Time())
@@ -97,7 +107,7 @@ class HumanFollower:
                     # This is where the target person's legs are                                      
                     legPosition = data.people[personIndex].pos
 
-                    rospy.loginfo("Computing target")
+                    rospy.loginfo("Computing goal")
 
                     # computing target point that is set distance away    
                     differenceX = legPosition.x - trans[0]
@@ -107,22 +117,34 @@ class HumanFollower:
                     angle = math.atan2(differenceY, differenceX)
                     length = math.hypot(differenceX, differenceY)
                     
+                    # calculating the position of the goal
                     target_length = length - DIST_FROM_TARGET
+                    goalX = target_length * math.cos(angle) + trans[0]
+                    goalY = target_length * math.sin(angle) + trans[1]
 
-                    targetX = target_length * math.cos(angle) + trans[0]
-                    targetY = target_length * math.sin(angle) + trans[1]
+                    # calculating the orientation quaternion of the goal
+                    quaternion = tf.transformations.quaternion_from_euler(0, 0, angle)
+
 
                     # forming target goal
                     target_goal_simple = PoseStamped()
-                    target_goal_simple.pose.position.x = targetX
-                    target_goal_simple.pose.position.y = targetY
+                    
+                    target_goal_simple.pose.position.x = goalX
+                    target_goal_simple.pose.position.y = goalY
                     target_goal_simple.pose.position.z = 0
-                    target_goal_simple.pose.orientation.w = 1
+                    
+                    target_goal_simple.pose.orientation.x = quaternion[0]
+                    target_goal_simple.pose.orientation.y = quaternion[1]
+                    target_goal_simple.pose.orientation.z = quaternion[2]
+                    target_goal_simple.pose.orientation.w = quaternion[3]
+
                     target_goal_simple.header.frame_id = 'map'
                     target_goal_simple.header.stamp = rospy.Time.now()
 
-                    # setting currently tracked objectID
+                    # setting currently tracked objectID, last known position
                     self.trackedObjectID = data.people[personIndex].object_id
+                    self.lastKnownPosition = target_goal_simple
+                    
                     # sending goal
                     if (self.previousGoal == None):
                         rospy.loginfo("first goal woo hoo!")
@@ -131,17 +153,16 @@ class HumanFollower:
                         pub.publish(target_goal_simple)
                     else:
                         #calculating distance from previous goal
-                        diffX = target_goal_simple.pose.position.x - self.previousGoal.pose.position.x
-                        diffY = target_goal_simple.pose.position.y - self.previousGoal.pose.position.y
-                        dist = math.hypot(diffX, diffY)
+                        distX = target_goal_simple.pose.position.x - self.previousGoal.pose.position.x
+                        distY = target_goal_simple.pose.position.y - self.previousGoal.pose.position.y
+                        dist = math.hypot(distX, distY)
                         
                         if (dist > DIST_FROM_PREVIOUS):
                             self.previousGoal = target_goal_simple
                             rospy.loginfo("sending new goal")
                             pub.publish(target_goal_simple)
                         else:
-                            rospy.loginfo("")
-                            rospy.loginfo("too close to current goal!")
+                            rospy.loginfo("new goal canceled: too close to current goal!")
 
                     # publish current position for visualization
                     positionPub.publish(curr_Position)
